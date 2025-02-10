70604a4f73d0c977e3b59c33acf1bb459e5a9767
nightfall-storm <bilalnnassernf@gmail.com>
2025-02-10 15:03:37 +0100

    fixed tailwind issues not applying

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,13 @@ export default {
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
+		"./public/**/*.{ts,tsx,html}",
+
 	],
 	theme: {
 		extend: {
