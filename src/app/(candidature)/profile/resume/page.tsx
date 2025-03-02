9353f84b91fcf6fe3957f31add96c5702c1a560a
nightfall-storm <bilalnnassernf@gmail.com>
2025-03-02 13:20:57 +0000

    readded previous page to fix the route name

---
+++ src/app/(candidature)/profile/resume/page.tsx
@@ -0,0 +1,5 @@
+import { QualificationsContainer } from "@/features/candidature/(profile)/components/qualifications/QualificationsContainer";
+
+export default function ResumePage() {
+  return (<QualificationsContainer />);
+}
