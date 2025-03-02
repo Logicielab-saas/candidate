1ae5bd218f8b2a61ae2aa8e58dc95cf058a27294
nightfall-storm <bilalnnassernf@gmail.com>
2025-03-02 13:20:04 +0000

    removed to adjust folder name

--- src/app/(candidature)/profile/Resume/page.tsx
+++
@@ -1,5 +0,0 @@
-import { QualificationsContainer } from "@/features/candidature/(profile)/components/qualifications/QualificationsContainer";
-
-export default function ResumePage() {
-  return (<QualificationsContainer />);
-}
