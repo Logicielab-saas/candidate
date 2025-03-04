e7eb098aa8441af8d65b3a97ffbdda310fbc487f
nightfall-storm <bilalnnassernf@gmail.com>
2025-03-04 11:58:09 +0000

    Add Formations page and update Navbar navigation
    - Create new Formations page route
    - Update Navbar to link to Formations page
    - Add avatar image to user profile in Navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -32,7 +32,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ThemeButton } from "@/components/shared/ThemeButton";
 import { usePathname } from "next/navigation";
 
@@ -48,7 +48,7 @@ import { usePathname } from "next/navigation";
 // }
 const navItems = [
   { name: "Home", url: "/", icon: Home },
-  { name: "Courses", url: "#", icon: User },
+  { name: "Formations", url: "/formations", icon: User },
   { name: "Profile", url: "/profile", icon: Briefcase },
 ];
 
@@ -166,7 +166,7 @@ export function NavBar() {
                         className="relative h-8 w-8 rounded-full"
                       >
                         <Avatar className="h-8 w-8">
-                          {/* <AvatarImage src="/avatars/01.png" alt="@USER" /> */}
+                          <AvatarImage src="/avatars/01.png" alt="@USER" />
                           <AvatarFallback>SC</AvatarFallback>
                         </Avatar>
                       </Button>
