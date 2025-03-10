0cf258ece3b861e76d1d9673eea1655b206ff889
nightfall-storm <bilalnnassernf@gmail.com>
2025-03-10 10:15:01 +0000

    Add JobApplyPage and JobApplyContainer for job application workflow

---
+++ src/app/(pages)/job-apply/page.tsx
@@ -0,0 +1,5 @@
+import { JobApplyContainer } from "@/features/job-apply/JobApplyContainer";
+
+export default function JobApplyPage() {
+  return <JobApplyContainer />;
+}
---
+++ src/features/job-apply/JobApplyContainer.tsx
@@ -0,0 +1,3 @@
+export function JobApplyContainer() {
+  return <div>JobApplyContainer</div>;
+}
